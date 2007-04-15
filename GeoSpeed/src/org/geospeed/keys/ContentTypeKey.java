1f7de443b28c0af74abe7e88f639f2170207dbd6
mike.abner <mike.abner@c4c0b656-011f-0410-a49e-35bfbcfcdccf>
2007-04-15 23:17:01 +0000

    added gif and png types

--- GeoSpeed/src/org/geospeed/keys/ContentTypeKey.java
+++ GeoSpeed/src/org/geospeed/keys/ContentTypeKey.java
@@ -5,7 +5,9 @@ public class ContentTypeKey extends Key
     public static final ContentTypeKey TEXT_PLAIN = new ContentTypeKey("text/plain");
     public static final ContentTypeKey TEXT_XML = new ContentTypeKey("text/xml");
     
+    public static final ContentTypeKey IMAGE_GIF = new ContentTypeKey("image/gif");
     public static final ContentTypeKey IMAGE_JPEG = new ContentTypeKey("image/jpg");
+    public static final ContentTypeKey IMAGE_PNG = new ContentTypeKey("image/png");
     
     private ContentTypeKey(String _name)
     {
