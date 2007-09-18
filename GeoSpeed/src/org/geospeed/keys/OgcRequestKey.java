9ae47de3949b87a0c534151d23894dc3b26b8210
mike.abner <mike.abner@c4c0b656-011f-0410-a49e-35bfbcfcdccf>
2007-09-18 16:04:16 +0000

    added httprequest headers

--- GeoSpeed/src/org/geospeed/keys/OgcRequestKey.java
+++ GeoSpeed/src/org/geospeed/keys/OgcRequestKey.java
@@ -6,7 +6,16 @@ public class OgcRequestKey extends Key
     public static final OgcRequestKey VERSION = new OgcRequestKey("VERSION");
     public static final OgcRequestKey WMTVER = new OgcRequestKey("WMTVER");
     public static final OgcRequestKey REQUEST = new OgcRequestKey("REQUEST");
-    public static final OgcRequestKey VendorParams = new OgcRequestKey("VENDORPARAMS");
+    public static final OgcRequestKey VENDORPARAMS = new OgcRequestKey("VENDORPARAMS");
+    public static final OgcRequestKey AUTHTYPE = new OgcRequestKey("AUTHTYPE");
+    public static final OgcRequestKey METHOD = new OgcRequestKey("METHOD");
+    public static final OgcRequestKey PATHINFO = new OgcRequestKey("PATHINFO");
+    public static final OgcRequestKey PATHTRANSLATED = new OgcRequestKey("PATHTRANSLATED");
+    public static final OgcRequestKey QUERYSTRING = new OgcRequestKey("QUERYSTRING");
+    public static final OgcRequestKey REMOTEUSER = new OgcRequestKey("REMOTEUSER");
+    public static final OgcRequestKey REQUESTEDSESSIONID = new OgcRequestKey("REQUESTEDSESSIONID");
+    public static final OgcRequestKey REQUESTURI = new OgcRequestKey("REQUESTURI");
+    public static final OgcRequestKey SERVLETPATH = new OgcRequestKey("SERVLETPATH");
     
     private OgcRequestKey(String _name)
     {
