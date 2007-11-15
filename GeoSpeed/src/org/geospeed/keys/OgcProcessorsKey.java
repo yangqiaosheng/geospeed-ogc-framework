fa92dcb9f1e328711795fe35049a22f6d5c9c395
mike.abner <mike.abner@c4c0b656-011f-0410-a49e-35bfbcfcdccf>
2007-11-15 19:00:03 +0000

    Added keys for WFS request types

--- GeoSpeed/src/org/geospeed/keys/OgcProcessorsKey.java
+++ GeoSpeed/src/org/geospeed/keys/OgcProcessorsKey.java
@@ -2,6 +2,7 @@ package org.geospeed.keys;
 
 public class OgcProcessorsKey extends Key
 {
+	//WMS
     public static final OgcProcessorsKey WMSGETCAPABILITIES = new OgcProcessorsKey("WmsGetCapabilitiesProcessor");
     public static final OgcProcessorsKey WMSGETMAP = new OgcProcessorsKey("WmsGetMapProcessor");
     public static final OgcProcessorsKey WMSGETFEATUREINFO = new OgcProcessorsKey("WmsGetFeatureInfoProcessor");
@@ -9,6 +10,14 @@ public class OgcProcessorsKey extends Key
     public static final OgcProcessorsKey WMSGETLEGENDGRAPHIC = new OgcProcessorsKey("WmsGetLegendGraphicProcessor");
     public static final OgcProcessorsKey WMSGETSTYLES = new OgcProcessorsKey("WmsGetStylesProcessor");
     public static final OgcProcessorsKey WMSPUTSTYLES = new OgcProcessorsKey("WmsPutStylesProcessor");
+    
+    //WFS
+    public static final OgcProcessorsKey WFSGETCAPABILITIES = new OgcProcessorsKey("WfsGetCapabilitiesProcessor");
+    public static final OgcProcessorsKey WFSDESCRIBEFEATURETYPE = new OgcProcessorsKey("WfsDescribeFeatureTypeProcessor");
+    public static final OgcProcessorsKey WFSGETFEATURE = new OgcProcessorsKey("WfsGetFeatureProcessor");
+    public static final OgcProcessorsKey WFSLOCKFEATURE = new OgcProcessorsKey("WfsLockFeatureProcessor");
+    public static final OgcProcessorsKey WFSTRANSACTION = new OgcProcessorsKey("WfsTransactionProcessor");
+    public static final OgcProcessorsKey WFSGETGMLOBJECT = new OgcProcessorsKey("WfsGetGmlObjectProcessor");
     
     public OgcProcessorsKey(String _name)
     {
